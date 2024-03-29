9c456d234d2c6ea42d513cccb4f8b2a00c982468
Fredy Carrillo Gutiérrez <fcarrillo@cosmocolor.com.mx>
2024-03-28 23:26:24 -0600

    'LogicwithoutData'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,13 +6,30 @@
       
       LunchCheckController.$inject= ['$scope'];
       function LunchCheckController($scope){
-        $scope.name = "";
+        $scope.food = "";
+        $scope.valueMessage = "";
 
         $scope.LunchMessage = function(){
+            $scope.valueMessage = Message($scope.food);
+        };
+
+        /*$scope.LunchMessage = function(){
+
+            //Enjoy
+            //To much
 
 
+        }*/
+
+        function Message(str){
+            var count = 0;
+            if(str == ""){
+                return "Please enter data first";
+            }
+            count = str.split(',')
+            console.log ('valor de count ' + count);
+            return "Sin evaluacui&oacute;n"
         }
-
 
 
       }
